42ba79d33d6fbce27d522ac17d7cc5fcab919241
vinuthnamanikonda7 <vinuthnamanikonda7@gmail.com>
2026-09-10 20:29:04 +0530

    Update database connection for deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
 
 const db = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "Password#2024",
-    database: "mindcare",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
@@ -13,7 +14,6 @@ const db = mysql.createPool({
 db.getConnection((err, connection) => {
 
     if (err) {
-        
         console.error("❌ MySQL connection failed:");
         console.error(err.message);
         return;
